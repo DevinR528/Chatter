3e193a4e767c8ba476b2647502ceee0d0fbfd5b6
devin.R[0] <devin.ragotzy@gmail.com>
2018-02-27 18:04:49 -0500

    index.html is roughed out css started server listens

--- server/server.js
+++ server/server.js
@@ -5,5 +5,11 @@ const https = require('http2');
 const path = require('path');
 
 const publicPath = path.join(__dirname, '/../public/');
+const port = process.env.PORT || 3000;
+var app = express();
 
-console.log(__dirname);
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+});
