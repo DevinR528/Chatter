7d3d42a3ee2894dddf8155cfc2f738695aebc253
devin.R[0] <devin.ragotzy@gmail.com>
2018-02-27 22:12:30 -0500

    changed folder struc, created msg event and listen

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+var socket = io();
+
+socket.on('connect', function() {
+    console.log('connected to server');
+
+    // client sends msg to server
+    socket.emit('createMsg', JSON.stringify({
+        text: 'hello server'
+    }));
+});
+
+socket.on('disconnect', function() {
+    console.log('disconnected from server');
+});
+
+socket.on('newMsg', function(msg) {
+    var clientMsg = JSON.parse(msg);
+    console.log(`New Message from: ${clientMsg.from} -> ${clientMsg.text}`);
+});
--- server/server.js
+++ server/server.js
@@ -18,8 +18,35 @@ app.use(express.static(publicPath));
  * @event connection on socket connection 
  */
 io.on('connection', (socket) => {
-    console.log('Server connected to client socket: ' + JSON.stringify(socket[0]));
+    console.log('Server connected to client socket: ' + socket[0]);
+
+    // server sends msg to client
+    socket.emit('newMsg', JSON.stringify({
+        from: 'Austin',
+        text: 'Hey',
+        timeStamp: 123
+    }));
+
+    /**
+     * @event createMsg when client fires createMsg server socket listens for it
+     */
+    socket.on('createMsg', (msg) => {
+        var parseMsg = JSON.parse(msg);
+        console.log(`Message from client ${parseMsg.text}`);
+    });
+
+    /**
+     * @event disconnected from client socket connection 
+     */
+    io.on('disconnect', (socket) => {
+        console.log('Server disconnected from client socket: ' + socket[0]);
+    });
+
+
 });
+
+
+
 
 /**
  * http server listens on port 
